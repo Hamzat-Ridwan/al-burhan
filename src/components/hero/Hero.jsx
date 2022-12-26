60b7a96ecff41dce4ae41e800d4835cd3aa7dd05
Ridwan <ridwanhamzat99@gmail.com>
2022-12-26 23:53:19 +0100

    feat: add slider

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
+import prev from '../../assets/prev.svg'
+import next from '../../assets/next.svg'
+import madinah from '../../assets/madinah.svg'
+import makkah from '../../assets/makkah.svg'
+import haram from '../../assets/haram.svg'
 import './Hero.css'
+import { useState } from 'react'
+
+const slider = [
+  {url: madinah},
+  {url: makkah},
+  {url: haram}
+]
 
 const Hero = () => {
+  const [slideIndex, setSlideIndex] = useState(1)
+
+    const nextSlide = () => {
+        if(slideIndex !== slider.length){
+            setSlideIndex(slideIndex + 1)
+        } 
+        else if (slideIndex === slider.length){
+            setSlideIndex(1)
+        }
+    }
+
+    const prevSlide = () => {
+        if(slideIndex !== 1){
+            setSlideIndex(slideIndex - 1)
+        }
+        else if (slideIndex === 1){
+            setSlideIndex(slider.length)
+        }
+        console.log(slideIndex)
+    }
   return (
     <div className='hero'>
-      <h3>THE BEST SUPPLICATION ON THE DAY OF ARAFAH</h3>
-      <button>Read more</button>
+      {slider.map((item, index)=> {
+        return(
+          <div className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+            style={{background: `linear-gradient(0deg, rgba(51, 51, 51, 0.6), rgba(51, 51, 51, 0.6)), url(${item.url})`}}
+          >
+            <h3>THE BEST SUPPLICATION ON THE DAY OF ARAFAH</h3>
+            <button>Read more</button>
+          </div>
+        )
+      })}
+      <div onClick={()=> prevSlide()}>
+        <img src={prev} className='prev' alt='arrow'/>
+      </div>
+      
+      <img src={next} className='next' onClick={nextSlide} alt='arrow'/>
     </div>
   )
 }
